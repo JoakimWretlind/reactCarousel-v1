9e257837d3001c0e191c6d3dbdb09e5077df7355
Joakim Wretlind <43047812+JoakimWretlind@users.noreply.github.com>
2021-04-14 15:45:33 +0200

    Add files via upload

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [ 
+    {
+        image: "https://source.unsplash.com/1600x900/?art, abstract"
+    },
+    {
+        image: "https://source.unsplash.com/1600x900/?art, street art"
+    },
+    {
+        image: "https://source.unsplash.com/1600x900/?art, urban"
+    },
+    {
+        image: "https://source.unsplash.com/1600x900/?art, graffiti"
+    },
+    {
+        image: "https://source.unsplash.com/1600x900/?art, color"
+    }
+]
